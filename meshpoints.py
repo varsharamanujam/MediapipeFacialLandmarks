b2401d3b4342cc5d03685e7110e3c68c56f8015d
varsharamanujam <60292603+varsharamanujam@users.noreply.github.com>
2022-05-02 11:19:25 +0530

    Update meshpoints.py

--- meshpoints.py
+++ meshpoints.py
@@ -5,11 +5,11 @@ from custom.iris_lm_depth import from_landmarks_to_depth
 from videosource import FileSource, WebcamSource
 import math
 
-map_face_mesh = mp.solutions.face_mesh
+map_face_mesh = mp.solutions.face_mesh # object for face mesh 
 
 
 #right iris indices
-RIGHT_IRIS =[ 474,475,476,477]
+RIGHT_IRIS =[ 474,475,476,477] 
 #left iris indices
 LEFT_IRIS =[ 469,470,471,472]
 
@@ -20,75 +20,89 @@ R_H_RIGHT = [263] #right eye right corner
 
 # Euclaidean distance 
 def euclaideanDistance(point, point1):
-    x, y = point.ravel()
-    x1, y1 = point1.ravel()
-    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    x, y = point.ravel() # ravel flattens the array
+    x1, y1 = point1.ravel() # ravel flattens the array
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2) # euclidean distance
     return distance
 
-def iris_position(iris_center, right_point, left_point):
-    center_to_right_distance = euclaideanDistance(iris_center, right_point)
-    center_to_left_distance = euclaideanDistance(iris_center, left_point)
-    total_distance = euclaideanDistance(right_point, left_point)
-    ratio = center_to_right_distance/total_distance
-    IrisPosition = ""
-    if ratio <= 0.42:
-        IrisPosition = "Right"
-    elif ratio > 2.7 and ratio <= 3.2:
-        IrisPosition = "Center"
-    else:
-        IrisPosition = "Left"
-    return IrisPosition, ratio
+def right_iris_position(iris_center, right_point, left_point):
+    center_to_right_distance = euclaideanDistance(iris_center, right_point) # distance between iris center and right eye corner
+    total_distance = euclaideanDistance(right_point, left_point) # distance between right eye corner and left eye corner
+    ratio = center_to_right_distance/total_distance # ratio between distance between iris center and right eye corner and distance between right eye corner and left eye corner
+    IrisPosition = "" # Iris position
+    if ratio <= 0.42: # if ratio is less than 0.42
+        IrisPosition = "Right" # Iris position is right
+    elif ratio > 2.7 and ratio <= 3.2: # if ratio is between 2.7 and 3.2
+        IrisPosition = "Center" # Iris position is center
+    else: # if ratio is greater than 3.2
+        IrisPosition = "Left" # Iris position is left
+    return IrisPosition, ratio # return Iris position and ratio
+
+
+def left_iris_position(iris_center, right_point, left_point): # same as right_iris_position
+    center_to_left_distance = euclaideanDistance(iris_center, left_point) # distance between iris center and left eye corner
+    total_distance = euclaideanDistance(right_point, left_point) # distance between right eye corner and left eye corner
+    ratio = center_to_left_distance/total_distance # ratio between distance between iris center and left eye corner and distance between right eye corner and left eye corner
+    IrisPosition = "" # Iris position
+    if ratio <= 0.42: # if ratio is less than 0.42
+        IrisPosition = "Left" # Iris position is left
+    elif ratio > 2.7 and ratio <= 3.2: # if ratio is between 2.7 and 3.2
+        IrisPosition = "Center" # Iris position is center   
+    else: # if ratio is greater than 3.2
+        IrisPosition = "Right" # Iris position is right
+    return IrisPosition, ratio # return Iris position and ratio
+
 
 # camera object 
 camera = cv2.VideoCapture(0)
 ret, frame = camera.read() # getting frame from camera 
 with map_face_mesh.FaceMesh(
-    max_num_faces = 1,refine_landmarks = True,  min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh:
+    max_num_faces = 1,refine_landmarks = True,  min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh: # face mesh object
     # starting Video loop here.
-    while True:
+    while True: # looping until break
         ret, frame = camera.read() # getting frame from camera 
         
-        if not ret: 
+        if not ret:  # if frame is not retrived
             break # no more frames break
         #flip the frame
         frame = cv2.flip(frame, 1)
-        img_h, img_w= frame.shape[:2]
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        results  = face_mesh.process(rgb_frame)
-        if results.multi_face_landmarks:
+        img_h, img_w= frame.shape[:2] # getting height and width of frame
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) # converting frame to BGR
+        results  = face_mesh.process(rgb_frame) # getting results from face mesh
+        if results.multi_face_landmarks: # if there are multiple faces
             meshpoints = np.array(
                 [
-                    np.multiply([p.x, p.y], [img_w, img_h]).astype(int)
-                    for p in results.multi_face_landmarks[0].landmark
+                    np.multiply([p.x, p.y], [img_w, img_h]).astype(int) # getting meshpoints
+                    for p in results.multi_face_landmarks[0].landmark # looping through landmarks
                 ]
             )
-            (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(meshpoints[LEFT_IRIS])
-            (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(meshpoints[RIGHT_IRIS])
-            center_left = np.array([l_cx, l_cy], dtype=np.int32)
-            center_right = np.array([r_cx, r_cy], dtype=np.int32)
+            (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(meshpoints[LEFT_IRIS]) # getting left iris center and radius
+            (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(meshpoints[RIGHT_IRIS]) # getting right iris center and radius
+            center_left = np.array([l_cx, l_cy], dtype=np.int32) # left iris center
+            center_right = np.array([r_cx, r_cy], dtype=np.int32) # right iris center
             
             #print(meshpoints[LEFT_IRIS])
             #print(meshpoints[RIGHT_IRIS])
             #print(meshpoints[L_H_LEFT])
             #print(meshpoints[L_H_RIGHT])
             #drawing circles on the eyes
-            cv2.circle(frame, center_left, int(l_radius), (0, 255, 0), 2,cv2.LINE_AA)
-            cv2.circle(frame, center_right, int(r_radius), (0, 255, 0), 2,cv2.LINE_AA)
-
-            cv2.circle(frame, meshpoints[R_H_RIGHT][0], 2, (255, 255, 255), 2,cv2.LINE_AA)
-            cv2.circle(frame, meshpoints[R_H_LEFT][0], 2, (255, 255, 255), 2,cv2.LINE_AA)
+            cv2.circle(frame, center_left, int(l_radius), (0, 255, 0), 2,cv2.LINE_AA) # left iris
+            cv2.circle(frame, center_right, int(r_radius), (0, 255, 0), 2,cv2.LINE_AA) # right iris
+ 
+            cv2.circle(frame, meshpoints[R_H_RIGHT][0], 2, (255, 255, 255), 2,cv2.LINE_AA) # right eye right corner
+            cv2.circle(frame, meshpoints[R_H_LEFT][0], 2, (255, 255, 255), 2,cv2.LINE_AA) # right eye left corner
             
-            cv2.circle(frame, meshpoints[L_H_RIGHT][0], 2, (255, 255, 255), 2,cv2.LINE_AA)
-            cv2.circle(frame, meshpoints[L_H_LEFT][0], 2, (255, 255, 255), 2,cv2.LINE_AA)
+            cv2.circle(frame, meshpoints[L_H_RIGHT][0], 2, (255, 255, 255), 2,cv2.LINE_AA) # left eye right corner
+            cv2.circle(frame, meshpoints[L_H_LEFT][0], 2, (255, 255, 255), 2,cv2.LINE_AA) # left eye left corner
             
-            iris_pos_l, ratio_l = iris_position(center_left, meshpoints[R_H_RIGHT], meshpoints[R_H_LEFT][0])
-            iris_pos_r, ratio_r = iris_position(center_right, meshpoints[L_H_RIGHT], meshpoints[L_H_LEFT][0])
+            iris_pos_r, ratio_r = right_iris_position(center_right, meshpoints[R_H_RIGHT], meshpoints[R_H_LEFT][0]) # getting right iris position and ratio
+            iris_pos_l, ratio_l = left_iris_position(center_left, meshpoints[L_H_RIGHT], meshpoints[L_H_LEFT][0]) # getting left iris position and ratio
             
-            cv2.putText(frame, "Iris Position Left: {} ratio: {} Iris Position Right: {} ratio: {}".format(iris_pos_l,ratio_l,iris_pos_r,ratio_r), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "Iris Position Left: {} ratio: {} Iris Position Right: {} ratio: {}".format(iris_pos_l,ratio_l,iris_pos_r,ratio_r), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) # putting text on frame
             
-        cv2.imshow('frame', frame)
-        key = cv2.waitKey(2)
-        if key==ord('q') or key ==ord('Q'):
-            break
-    cv2.destroyAllWindows()
-    camera.release()
+        cv2.imshow('frame', frame) # showing frame
+        key = cv2.waitKey(2) # waiting for key press
+        if key==ord('q') or key ==ord('Q'): # if key is q or Q
+            break 
+    cv2.destroyAllWindows() # destroying all windows
+    camera.release() # releasing camera
